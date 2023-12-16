5a04253f549e6ef6cf9d208b8a891afec09e587c
wani1009 <syazwani10@outlook.com>
2023-12-16 13:23:59 +0800

    added changes

--- board.java
+++ board.java
@@ -2,5 +2,6 @@ public class board {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("code");
+        System.out.println("Hi123");
     }
 }
