b94afc6032ccc8d9ed20cc5652a0359ebbbd42fe
wani1009 <syazwani10@outlook.com>
2023-12-16 13:10:33 +0800

    Attempt to use JFrame

--- board.java
+++ board.java
@@ -1,7 +1,17 @@
-public class board {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class board extends JFrame{
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Testing");
         System.out.println("Testing testing...");
+        JFrame GUI = new JFrame();
+        JLabel title = new JLabel();
+        GUI.setSize(800, 600);
+        GUI.setVisible(true);
+        GUI.add(title);
+
     }
 }
+
+
